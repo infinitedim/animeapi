2c11475edbd2e73126c83234f9af6e2f93915525
infinitedim <saputradimas.boediluhur@gmail.com>
2022-10-31 05:17:47 +0700

    fix controllers problem

--- controllers/controllers.ts
+++ controllers/controllers.ts
@@ -1,11 +1,33 @@
-/* eslint-disable no-useless-catch */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Request, Response } from "express";
+import { Result, ValidationError, validationResult } from "express-validator";
+import { unlink } from "fs";
 import path from "path";
-import { ValidationError, validationResult, Result } from "express-validator";
-import { Request, Response, NextFunction } from "express";
-import { PathLike, unlink } from "fs";
 import AnimeModel from "../models/models";
+
+class CustomError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode?: number) {
+    super(message);
+    this.statusCode = statusCode !== undefined ? statusCode : 500;
+  }
+}
+
+function sendCustomError(res: Response, error: CustomError): Response {
+  console.error(error);
+
+  return res.status(error?.statusCode ?? 500).json({
+    code: error?.statusCode ?? 500,
+    message: error?.message ?? "Internal Server error",
+    status: "ERROR",
+  });
+}
+
+function deletetrailer(filePath: string): void {
+  filePath = path.join(`${__dirname}../${filePath}`);
+
+  unlink(filePath, (e) => console.error(e));
+}
 
 interface resultsInterface {
   trailer?: string;
@@ -15,17 +37,9 @@ interface resultsInterface {
   year?: string;
 }
 
-interface ErrorInterface {
-  code: number;
-  data: unknown;
-}
-
-interface EditAnimeRequestType extends Request {
-  file?: ReturnType<typeof File>;
-}
-
 const get = async (req: Request, res: Response): Promise<void> => {
   const data = await AnimeModel.find();
+
   res.status(200).json({
     status: "OK",
     messege: "Successfully get data",
@@ -34,73 +48,91 @@ const get = async (req: Request, res: Response): Promise<void> => {
   });
 };
 
-const post = (
-  req: Request, res: Response,
-): void => {
-  const error: Result<ValidationError> = validationResult(req);
-
-  if (!error.isEmpty()) {
-    const e: ErrorInterface = new Error("Invalid value");
-    e.code = 400;
-    e.data = error.array();
-    throw e;
-  }
-
-  if (req.file != null) {
-    const e: ReturnType<typeof Error> & ErrorInterface = new Error(
-      "Files must be upload",
-    );
-    e.code = 422;
-    throw e;
-  }
-
-  const results: resultsInterface = {
-    trailer: req.file?.path ?? "", 
-    title: req.body.title,
-    sinopsys: req.body.sinopsys,
-    author: req.body.author,
-    year: req.body.year,
-  };
-
-  const posting = new AnimeModel(results);
-
-  posting
-    .save()
-    .then((response): void => {
-      res.status(201).json({
-        messege: "Successfully post new data",
-        code: 201,
-        data: response,
-      });
-    })
-    .catch((e) => console.error(e));
-};
-
-const search = async (req: Request, res: Response): Promise<void> => {
-  const animeId: string = req.params.id;
-  const data = await AnimeModel.findById(animeId);
-
-  res.status(200).json({
-    code: 200,
-    status: "OK",
-    messege: "Successfully get data by id",
-    data,
-  });
-};
-
-const edit = async (req: EditAnimeRequestType, res: Response): Promise<Response> => {
+const post = async (
+  req: Request<undefined, unknown, resultsInterface>,
+  res: Response,
+): Promise<Response> => {
   const error: Result<ValidationError> = validationResult(req);
 
   if (!error.isEmpty()) {
     throw new Error("Invalid value");
   }
 
-  if (req.file != null) {
+  if (req.file == null || req.file === undefined) {
     throw new Error("Files must be upload");
   }
 
   const results: resultsInterface = {
-    trailer: req.file?.path ?? "", // ini possible undefined req.file nya
+    trailer: "path" in req.file ? req.file?.path : "",
+    title: req.body.title,
+    sinopsys: req.body.sinopsys,
+    author: req.body.author,
+    year: req.body.year,
+  };
+
+  try {
+    const posting = new AnimeModel(results);
+    const savedAnime = await posting.save();
+
+    if (savedAnime === null || savedAnime === undefined) {
+      throw new CustomError("Error when posting anime");
+    }
+
+    return res.status(201).json({
+      code: 201,
+      message: "Successfully post new data",
+      status: "OK",
+      data: savedAnime,
+    });
+  } catch (error) {
+    if (error instanceof CustomError) {
+      return sendCustomError(res, error);
+    }
+
+    throw error;
+  }
+};
+
+const search = async (
+  req: Request<{ id: string }>,
+  res: Response,
+): Promise<Response> => {
+  const { id } = req.params;
+
+  try {
+    const data = await AnimeModel.findById(id);
+
+    return res.status(200).json({
+      code: 200,
+      status: "OK",
+      messege: "Successfully get data by id",
+      data,
+    });
+  } catch (error) {
+    if (error instanceof CustomError) {
+      return sendCustomError(res, error);
+    }
+
+    throw error;
+  }
+};
+
+const edit = async (
+  req: Request<{ id: string }, unknown, resultsInterface>,
+  res: Response,
+): Promise<Response> => {
+  const error: Result<ValidationError> = validationResult(req);
+
+  if (!error.isEmpty()) {
+    throw new Error("Invalid value");
+  }
+
+  if (req.file == null || req.file === undefined) {
+    throw new Error("Files must be upload");
+  }
+
+  const results: resultsInterface = {
+    trailer: "path" in req.file ? req.file?.path : "", // ini possible undefined req.file nya
     title: req.body.title,
     sinopsys: req.body.sinopsys,
     author: req.body.author,
@@ -109,14 +141,13 @@ const edit = async (req: EditAnimeRequestType, res: Response): Promise<Response>
 
   try {
     const { id } = req.params;
+
     const check = await AnimeModel.findOne({
       _id: id,
     });
 
     if (check == null) {
-      return res.status(404).json({
-        // ...code
-      });
+      throw new CustomError("Anime not found", 404);
     }
 
     const updatedAnime = await AnimeModel.findOneAndUpdate(
@@ -139,44 +170,61 @@ const edit = async (req: EditAnimeRequestType, res: Response): Promise<Response>
         status: "ERROR",
         code: 500,
         messege: "...",
-        data: response,
       });
     }
 
     return res.status(200).json({
-      // ...code
+      status: "OK",
+      code: 200,
+      message: "Data updated successfully",
+      data: updatedAnime,
     });
   } catch (error) {
+    if (error instanceof CustomError) {
+      return sendCustomError(res, error);
+    }
+
     throw error;
   }
 };
 
-const del = (req: Request, res: Response, next: NextFunction): void => {
-  const animeId: string = req.params.id;
-  AnimeModel.findById(animeId)
-    .then((response): void => {
-      if (response == null) {
-        const err = new Error("Anime not found!");
-        err.code(404);
-      }
+const del = async (
+  req: Request<{
+    id: string;
+  }>,
+  res: Response,
+): Promise<Response> => {
+  const { id } = req.params;
 
-      deletetrailer(response.trailer);
-      return AnimeModel.findByIdAndRemove(animeId);
-    })
-    .then((response): void => {
-      res.status(200).json({
-        status: "OK",
-        code: 200,
-        messege: "Data deleted",
-        data: response,
-      });
-    })
-    .catch((e) => next(e));
-};
+  try {
+    const anime = await AnimeModel.findById(id);
 
-const deletetrailer = (filePath: PathLike): void => {
-  filePath = path.join(`${__dirname}../${filePath}`);
-  unlink(filePath, (e) => console.error(e));
+    if (anime == null) {
+      throw new CustomError("Anime not found", 404);
+    }
+
+    const deletedAnime = await AnimeModel.findOneAndDelete({
+      _id: id,
+    });
+
+    if (deletedAnime == null) {
+      throw new CustomError("Error when deleting anime", 500);
+    }
+
+    deletetrailer(deletedAnime.trailer);
+
+    return res.status(200).json({
+      status: "OK",
+      code: 200,
+      message: "Data deleted",
+    });
+  } catch (error) {
+    if (error instanceof CustomError) {
+      return sendCustomError(res, error);
+    }
+
+    throw error;
+  }
 };
 
 export { get, post, search, edit, del };
